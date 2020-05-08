89780cf09dcf335e0e6dbf428077583846a86ced
wanzhip <1151333166@qq.com>
2020-05-08 19:14:27 +0800

    2.1

--- dist/dictionary.js
+++ dist/dictionary.js
@@ -1,9 +1,11 @@
-"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class Dictionary {
     constructor() {
         this.keys = [];
         this.vals = [];
+    }
+    get size() {
+        return this.keys.length;
     }
     set(key, val) {
         let i = this.keys.indexOf(key);
@@ -14,8 +16,31 @@ class Dictionary {
             this.keys.push(key);
             this.vals.push(val);
         }
-        console.log(this.keys);
-        console.log(this.vals);
+    }
+    get(k) {
+        const i = this.keys.indexOf(k);
+        if (i < 0) {
+            return undefined;
+        }
+        return this.vals[i];
+    }
+    forEach(callback) {
+        this.keys.forEach((k, i) => {
+            const v = this.vals[i];
+            callback(k, v);
+        });
+    }
+    has(k) {
+        return this.keys.includes(k);
+    }
+    delete(k) {
+        const i = this.keys.indexOf(k);
+        if (i < 0) {
+            return;
+        }
+        this.keys.splice(i, 1);
+        this.vals.splice(i, 1);
     }
 }
 exports.Dictionary = Dictionary;
+Map;
--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,17 @@
-"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const dictionary_1 = require("./dictionary");
 const dic = new dictionary_1.Dictionary();
 dic.set("a", 1);
+dic.set("a", 11);
+dic.set("b", 22);
+console.log(dic.size);
+dic.forEach((k, v) => {
+    console.log(`${k}:${v}`);
+});
+console.log(dic.has("a"));
+console.log(dic.delete("a"));
+dic.forEach((k, v) => {
+    console.log(`${k}:${v}`);
+});
+console.log(dic.size);
+console.log(dic.get("b"));
